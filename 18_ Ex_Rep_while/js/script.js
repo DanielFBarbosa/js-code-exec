f7fbc48d502dd97d7f4b51c13d29e26c80adf967
Daniel Ferreira Barbosa <125709368+DanielFBarbosa@users.noreply.github.com>
2023-04-11 00:33:41 -0300

    ATUALIZEI

---
+++ 18_ Ex_Rep_while/js/script.js
@@ -0,0 +1,19 @@
+var contador = 0
+while(contador < 15){
+    console.log(contador)
+    contador++
+}
+
+var x = 2
+while(x > 0){
+    console.log(x)
+    x--
+}
+
+var y = 0
+var x =10
+while(y < 10 && x > 0){
+    console.log(y,x)
+    y++
+    x--
+}          
