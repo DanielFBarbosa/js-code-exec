8525f62a92877178f47461739b429a544015e566
Daniel Ferreira Barbosa <125709368+DanielFBarbosa@users.noreply.github.com>
2023-04-21 20:21:25 -0300

    atualizando

---
+++ 37_remover_elementos/js/script.js
@@ -0,0 +1,11 @@
+//Removendo elemento filho com removeChild
+var container = document.querySelector("#container");
+
+var p = document.querySelector("#container p");
+
+container . removeChild(p);
+
+//removendo elemento pai com remove apenas
+var subtitle = document.querySelector(".subtitle");
+
+subtitle.remove();
