28f609c4b2080080a4b44e79c9b83ccb6260c6a6
Daniel Ferreira Barbosa <125709368+DanielFBarbosa@users.noreply.github.com>
2023-04-24 23:29:17 -0300

    atualizei

---
+++ 44_setTimeOut_setInterval/js/script.js
@@ -0,0 +1,14 @@
+//setTimeOut
+console.log("Antes do setTimeOut");
+setTimeout(function(){
+
+    console.log("Testando o setTimeOut")
+
+}, 4000);
+
+console.log("Depois do setTimeOut");
+
+//setInterval
+setInterval(function(){
+    console.log("Testando setInterval")
+}, 3000)
